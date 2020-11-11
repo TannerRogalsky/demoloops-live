4f89b928748d7cb2d4c5b1cae344e9005c87b21c
Tanner Rogalsky <tanner@tannerrogalsky.com>
2020-11-11 16:58:37 -0500

    styling mostly

--- js/index.js
+++ js/index.js
@@ -12,6 +12,8 @@ import "codemirror/addon/fold/foldgutter.css";
 import "codemirror/addon/search/match-highlighter";
 import "codemirror/addon/selection/active-line";
 
+import "./styles.css";
+
 const DEFAULT_LOOP = `\
 fn update(ratio, width, height) {
     let ctx = Context();
@@ -34,16 +36,41 @@ fn update(ratio, width, height) {
 
     let black = Color(0.0, 0.0, 0.0, 1.0);
 
-    for y in range(0, 2) {
-        for x in range(0, 2) {
+    for y in range(0, 10) {
+        for x in range(0, 10) {
             let x_offset = (y % 2).to_float() * 0.5;
             let x_offset = x.to_float() + x_offset;
             let x = x_offset * radius * 2.0;
             let y = y.to_float() * apothem * 2.0;
-
+            
             let origin = Translation(x, y);
             ctx.draw(central_shape, origin, central_color);
             ctx.stroke(central_shape, origin, black);
+        }
+    }
+
+    for y in range(0, 10) {
+        for x in range(0, 10) {
+            let x_offset = (y % 2).to_float() * 0.5;
+            let x_offset = x.to_float() + x_offset;
+            let x = x_offset * radius * 2.0;
+            let y = y.to_float() * apothem * 2.0;
+            
+            let origin = Translation(x, y);
+
+            for i in range(0, n) {
+                let inner_ratio = i.to_float() / n.to_float();
+                let mu = inner_ratio * 360.0;
+                let outer_transform = origin 
+                    * Rotation(mu)
+                    * Translation(radius + inner_radius, 0.0)
+                    * Rotation(interior_angle)
+                    * Translation(side * ratio, 0.0)
+                    ;
+
+                ctx.draw(outer_shape, outer_transform, outer_color);
+                // ctx.stroke(outer_shape, outer_transform, black);
+            }
         }
     }
 
@@ -110,7 +137,10 @@ let demoloop = new Demoloop();
 demoloop.update(DEFAULT_LOOP);
 
 editor.on('change', (editor, changes) => {
-    demoloop.update(editor.getValue());
+    const result = demoloop.update(editor.getValue());
+    if (result) {
+        console.error(result);
+    }
 });
 
 let loop = () => {
